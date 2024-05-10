19d7d68812c389d4e65bcb62d8cc2f786391bb82
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-10 12:47:23 -0300

    feat: create user-list-component

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-users-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './users-list.component.html',
-  styleUrl: './users-list.component.scss'
+  templateUrl: './users-list.component.html'
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    }, error => console.log(error));
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(data => {
+      this.loadUsers();
+    }, error => console.log(error));
+  }
+
+  loadUsers() {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
