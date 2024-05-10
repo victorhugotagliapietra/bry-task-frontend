579b40b8a83e8d128da5eb7557c8c6aec4aba3de
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-10 13:11:16 -0300

    fix: update file name and add NgModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersListComponent },
   { path: 'users/create', component: UserFormComponent },
   { path: 'users/:id', component: UserDetailsComponent },
   { path: 'users/:id/edit', component: UserFormComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
