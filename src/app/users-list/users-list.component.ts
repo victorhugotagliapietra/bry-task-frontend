cbc39298c2253bff6c87dc16a38acb732eddf6ae
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-10 15:12:02 -0300

    refactor: update functions

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users-list',
@@ -11,23 +12,25 @@ import { User } from '../user';
 export class UsersListComponent implements OnInit {
   users: User[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data;
-    }, error => console.log(error));
+    this.loadUsers();
+  }
+
+  editUser(id: number) {
+    this.router.navigate(['/users', id, 'edit']);
   }
 
   deleteUser(id: number) {
-    this.userService.deleteUser(id).subscribe(data => {
-      this.loadUsers();
-    }, error => console.log(error));
+    this.userService.deleteUser(id).subscribe(() => this.loadUsers());
   }
 
   loadUsers() {
-    this.userService.getUsers().subscribe(data => {
-      this.users = data;
-    });
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
+
+  addUser() {
+    this.router.navigate(['/users/create']);
   }
 }
