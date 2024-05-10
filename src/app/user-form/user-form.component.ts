986f92367d843a2f38e4d9c77f8fa2c349346b77
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-10 13:16:21 -0300

    feat: create user form component

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
+  templateUrl: './user-form.component.html'
 })
-export class UserFormComponent {
+export class UserFormComponent implements OnInit {
+  user: User = new User();
 
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.userService.getUser(+id).subscribe(data => {
+        this.user = data;
+      });
+    }
+  }
+
+  onSubmit() {
+    if (this.user.id) {
+      this.userService.updateUser(this.user.id, this.user).subscribe(result => this.gotoUserList());
+    } else {
+      this.userService.createUser(this.user).subscribe(result => this.gotoUserList());
+    }
+  }
+
+  gotoUserList() {
+    this.router.navigate(['/users']);
+  }
 }
