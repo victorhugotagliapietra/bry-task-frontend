8ec6baab160a08a36543381fdf0e3dbd0763cfb3
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-10 12:22:22 -0300

    feat: create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'users', component: UsersListComponent },
+  { path: 'users/create', component: UserFormComponent },
+  { path: 'users/:id', component: UserDetailsComponent },
+  { path: 'users/:id/edit', component: UserFormComponent }
+];
