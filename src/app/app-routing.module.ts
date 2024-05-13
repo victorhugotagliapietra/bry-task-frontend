698ff5e1a8ad3e013d79bbb89338d684b88e3f21
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 12:38:23 -0300

    fix: update imports and ad redirect to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersListComponent } from './users-list/users-list.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
+import { UsersListComponent } from './user/users-list/users-list.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '**', redirectTo: '/users' },
   { path: 'users', component: UsersListComponent },
   { path: 'users/create', component: UserFormComponent },
   { path: 'users/:id', component: UserDetailsComponent },
