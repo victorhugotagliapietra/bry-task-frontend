24233874155a96b4f033d67b3bf8779cb38bf670
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 12:22:40 -0300

    fix: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 @NgModule({
+  declarations: [
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
+  bootstrap: []
 })
 export class AppModule { }
