a0d4d9b49ad0aac067edf2c24dbbb7293c69fa49
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 14:28:11 -0300

    fix: update standalone and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule]
 })
 export class AppComponent {
   title = 'bry-task-frontend';
