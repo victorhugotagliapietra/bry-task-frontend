eef5816446ec2d115d4847f22c0927e1504f1101
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 12:37:51 -0300

    fix: update imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    UserModule
   ],
   providers: [],
   bootstrap: []
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserRoutingModule } from '../app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
 
 import { UsersListComponent } from './users-list/users-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
@@ -17,7 +17,7 @@ import { UserDetailsComponent } from './user-details/user-details.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    UserRoutingModule
+    AppRoutingModule
   ]
 })
 export class UserModule { }
