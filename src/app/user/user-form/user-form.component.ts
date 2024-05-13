dd3ad62becdb3aabf8e4104d2cf2f017082a7c15
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 14:06:01 -0300

    fix: add standalone and imports to user form component

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { User } from '../user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-user-form',
-  templateUrl: './user-form.component.html'
+  templateUrl: './user-form.component.html',
+  imports: [FormsModule],
 })
 export class UserFormComponent {
   user: User = new User();
