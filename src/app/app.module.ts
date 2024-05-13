ff0a9cec7b62000688228029d811deebc5bcad24
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-13 13:49:19 -0300

    fix: update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserModule } from './user/user.module';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full' },  // Redireciona a rota raiz para '/users'
+  { path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },  // Lazy loading para o módulo UserModule
+  { path: '**', redirectTo: '/users' }  // Rota de fallback para '/users'
+];
 
 @NgModule({
   declarations: [
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    UserModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: []
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
